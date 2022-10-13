9a862dcfa6e129f0c104db56373b1a6e6db0f302
jackson-ruiz <jackson.ruiz@thoughtworks.com>
2022-10-13 16:39:08 -0500

    Add existing project files to Git

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Sample from "./components/Sample";
+
+import "./index.scss";
+
+const App = () => <Sample />;
+ReactDOM.render(<App />, document.getElementById("app"));
